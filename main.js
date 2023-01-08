ff55f2fd18bb3335ce82e59421fe5646ce464a08
Adel <ledasaneca30@gmail.com>
2023-01-08 17:53:23 +0800

    My First Project(Module-1)

--- main.js
+++ main.js
@@ -18,19 +18,26 @@ export var newsList = [
 
 export function search() {
   //inputValue is the variable that contains the search string
-  const inputValue = document.getElementById("search-input").value;
-
-  //Write your code here for the search function
-
+  const inputValue = document.getElementById("search-input").value; // edit this line for testing or put slash to test in main.test.js, delete slash for Live Server.
+  
+  //const inputValue = "news_list"; //put slash after testing from main.test.js, and to test for Live Server. Then delete slash for testing in main.test.js
+  let searchNews;
+  for(let i = 0; i < newsList.length; i++){
+    let lowerCaseNewsList = newsList[i].toLowerCase().replace(/[\W_]/g,"");
+    let lowerCaseInputValue = inputValue.toLowerCase().replace(/[\W_]/g,"");
+    if(lowerCaseNewsList.includes(lowerCaseInputValue)){
+      return searchNews = newsList.filter( text => text.toLowerCase().replace(/[\W_]/g,"").includes(lowerCaseInputValue) ); //check if the inputValue exist in the newsList
+    }
+  }
   return newsList;
-}
+};
 
 export function sort(type) {
   if (type == "ascending") {
     //Write your code here for sorting (ascending)
+    return newsList.sort();
   } else {
     //Write your code here for sorting (descending)
+    return newsList.sort().reverse();
   }
-
-  return newsList;
-}
+};
